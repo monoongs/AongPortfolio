64dfb65e06a29a79caa472c1aab500b0d7a49064
monoongs <monoongs@gmail.com>
2020-05-29 23:25:12 +0700

    updated

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -6,9 +6,24 @@ class Intro extends Component {
   }
   render() {
     return(
-      <div className="bg-black flex justify-center items-center min-h-screen text-white">
-        Intro
+      // <div className="bg-black flex justify-center items-center min-h-screen text-white">
+      <div className="bg-gray-900 min-h-screen">
+        <div className="
+            flex flex-row bg-gray-900 min-h-screen items-center justify-center mx-auto max-w-screen-xs
+            sm:max-w-screen-sm
+            lg:max-w-screen-xl
+          ">
+          <div className="bg-gray-500 w-3/5 text-right">
+            <p>Hello</p>
+            <p>I'am Web Developer</p>
+          </div>
+          <div className="bg-gray-400 w-2/5">
+            2
+          </div>
+        </div>
+
       </div>
+
     )
   }
 }
