95ddb67ee0dfe16f94b42999fa85a43d2265a4d0
monoongs <monoongs@gmail.com>
2020-05-31 00:32:21 +0700

    updated

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -8,21 +8,22 @@ class Intro extends Component {
     return(
       // <div className="bg-black flex justify-center items-center min-h-screen text-white">
       <div className="bg-gray-900 min-h-screen">
-        <div className="
-            flex flex-row bg-gray-900 min-h-screen items-center justify-center mx-auto max-w-screen-xs
-            sm:max-w-screen-sm
-            lg:max-w-screen-xl
-          ">
-          <div className="bg-gray-500 w-3/5 text-right">
-            <p>Hello</p>
-            <p>I'am Web Developer</p>
+      <div className="
+          flex bg-gray-900 min-h-screen items-center justify-center mx-auto max-w-screen-xs
+          sm:max-w-screen-sm
+          lg:max-w-screen-xl
+        ">
+        <div className="flex flex-col sm:flex-col md:flex-row lg:flex-row items-center">
+          <div className="w-1/2 text-right z-10 lg:mr-ne14">
+            <h1 className="text-6xl text-white">Hello</h1>
+            <h2 className="text-4xl text-white">I'am Web Developer</h2>
           </div>
-          <div className="bg-gray-400 w-2/5">
-            2
+          <div className="w-1/2 py-5">
+            <img src="./img/gentleman.jpg" />
           </div>
-        </div>
-
+        </div>         
       </div>
+    </div>
 
     )
   }
