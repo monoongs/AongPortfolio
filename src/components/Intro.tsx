d0ff616957d48332e4d764fb321796879d2d686f
monoongs <monoongs@gmail.com>
2020-05-31 13:57:09 +0700

    intro success

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -13,10 +13,10 @@ class Intro extends Component {
           sm:max-w-screen-sm
           lg:max-w-screen-xl
         ">
-        <div className="flex flex-col sm:flex-col md:flex-row lg:flex-row items-center">
-          <div className="w-1/2 text-right z-10 lg:mr-ne14">
-            <h1 className="text-6xl text-white">Hello</h1>
-            <h2 className="text-4xl text-white">I'am Web Developer</h2>
+        <div className="flex flex-col-reverse md:flex-row lg:flex-row items-center">
+          <div className="w-1/2 z-10 md:mr-ne8 lg:mr-ne14">
+            <h1 className="text-4r text-center md:text-right md:text-7r lg:text-9r text-white">Hello</h1>
+            <h2 className="text-1.35r text-center md:text-right md:text-2r lg:text-3.25r text-white">I'am Web Developer</h2>
           </div>
           <div className="w-1/2 py-5">
             <img src="./img/gentleman.jpg" />
