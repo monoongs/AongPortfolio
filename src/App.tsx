5752216cb77cbd0a04c0e36e97e0f7f5024d9772
monoongs <monoongs@gmail.com>
2020-06-01 23:17:53 +0700

    design second section

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import "./styles/tailwind/main.css";
 import './App.css';
 import Intro from './components/Intro'
+import AboutMe from './components/AboutMe'
 
 class App extends Component {
   render() {
     return(
-      <Intro />
+      <>
+        <Intro />
+        <AboutMe />
+      </>
     )
   }
 }
